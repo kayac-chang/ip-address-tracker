22b8902cffa9fd40135fca2b0c9bf665af5e4317
kayac-chang <egg734631@gmail.com>
2021-09-21 18:42:28 +0800

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Geolocation, Location } from "./api";
 import { Map, Search, Data } from "./components";
 import { isIP, isFQDN } from "validator";
 import { URLSearchParams } from "./utils";
 
-function search(terms: string) {
-  if (isFQDN(terms)) return Geolocation.search({ domain: terms });
-
-  if (isIP(terms)) return Geolocation.search({ ipAddress: terms });
-
-  return Geolocation.search();
-}
-
 function App() {
   const [location, setLocation] = useState<Location>();
 
-  useEffect(() => {
-    const terms = URLSearchParams(window.location.search).search || "";
+  const search = useCallback(
+    (terms) => {
+      if (isFQDN(terms))
+        return Geolocation.search({ domain: terms }).then(setLocation);
 
-    search(terms).then(setLocation);
+      if (isIP(terms))
+        return Geolocation.search({ ipAddress: terms }).then(setLocation);
+
+      return Geolocation.search().then(setLocation);
+    },
+    [setLocation]
+  );
+
+  useEffect(() => {
+    search(URLSearchParams(window.location.search).search || "");
   }, []);
 
   return (
